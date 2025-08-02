5ca3945c2a407357faf66c0d3364eabb33034ecb
hojung7 <wjdghwjd99@naver.com>
2025-08-02 20:14:21 +0900

    [level 0] Title: 문자열 출력하기, Time: 16.42 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        string str = Console.ReadLine(); // 입력 받기
+        Console.WriteLine(str); // 입력받은 문자열 출력
+    }
+}
